dbd4c41a9027f9193342522653c489e72b250ff5
kowshickraj21 <kowshickraj21.2005@gmail.com>
2023-08-09 14:05:05 +0530

    Third commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+let sum = 0;
+let cards=[];
 function start(){
     let random= Math.random()*6+1;
-    let sum= Math.round(random);
+    let card1= Math.round(random);
+    let card2= Math.round(random);
+    cards.push(card1);
+    cards.push(card2);
+    document.getElementById("cards").innerHTML="Cards: " + cards;
+    for (let i=0; i<cards.length; i++) {
+    sum = sum + cards[i];
+}
     document.getElementById("sum").innerHTML="Sum: " + sum;
+}
+
+function draw(){
+    cards.push(Math.round(Math.random()*6+1));
 }
